46f26d70a87ccb8dcd94764908ccc646028dd759
Christian Garcia <bones192nd@gmail.com>
2023-01-10 20:54:56 -0500

    Lab entry

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+// multiply
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+
+// random
+const random = Math.floor(Math.random() * 10)+1;
+
+
+// mod
+
+let num3 = 19;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+
+// max
+
+const max = Math.max(0, 3, 5, 10, 19, 20);
